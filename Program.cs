a848566cd6b9ea010ff3515351ac498a2892fc59
Kacper Dremlik <kacper.dremlik@gmail.com>
2021-11-07 14:35:43 +0100

    Added feature ControlSum.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] wagi = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+            string pesel = "49040501580";
+            Console.WriteLine(pesel);
+            Console.WriteLine(ControlSum(pesel, wagi));
+            
+        }
+
+        public static int ControlSum(string pesel, int[] wagi)
+        {
+            int[] id = new int[pesel.Length];
+            int sum = 0;
+            for (int i = 0; i < pesel.Length -1; i++)
+            {
+                sum += wagi[i] * id[i];
+            }
+
+            int s = sum % 10;
+
+            if (s == 0)
+                return 0;
+            return 10 - s;
+
+
         }
     }
 }
+//E09
+
+//Dla numeru PESEL podanego jako tekst, sprawdź, czy ma on poprawną cyfrę kontrolną zgodnie z
+//algorytmem podanym na stronie
+//pl.wikipedia.org/wiki/PESEL#Cyfra_kontrolna_i_sprawdzanie_poprawności_numeru
+//Liczby, przez które mnożymy poszczególne cyfry numeru PESEL nazywają się wagami.
+//Utwórz tablicę, w której zapiszesz te wagi i wykorzystaj tę tablicę do sprawdzenia poprawności
+//cyfry kontrolnej.
