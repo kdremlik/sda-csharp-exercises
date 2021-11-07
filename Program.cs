841b6d24a8dc59ccf7880bf9432b0cf5ee36fea8
Kacper Dremlik <kacper.dremlik@gmail.com>
2021-11-07 10:51:16 +0100

    Added branch E03 with new feature.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+		public static void ArithmeticSequence(int[] sequ)
+        {
+            Console.WriteLine("Ciąg: ");
+            int printValue = sequ[1];
+            Console.Write(printValue);
+            for (int i = 1; i < sequ[0]; i++)
+            {
+                printValue += sequ[2];
+                Console.Write($", {printValue}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Długość ciągu: ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pierwsza wartość: ");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Różnica: ");
+            int difference = Convert.ToInt32(Console.ReadLine());
+
+            int[] sequ = { lenght, firstValue, difference };
+            ArithmeticSequence(sequ);
         }
-    }
+	}
 }
