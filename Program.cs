6a98a261532dab989fc2f92e3c4e68271a9a0dc3
Kacper Dremlik <kacper.dremlik@gmail.com>
2021-11-07 11:29:54 +0100

    Added feature Factorial.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj wartość:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x > 0)
+            {
+                Console.WriteLine(Factorial(x));
+            }
+            else
+            {
+                Console.WriteLine("Liczba nie może być mniejsza od 0!");
+            }
+            
+            
         }
+
+        static int Factorial(int x)
+        {
+
+                if (x == 0)
+                {
+                    return 1;
+
+                }
+
+                int result = x * Factorial(x - 1);
+                return result;
+            }
     }
 }
