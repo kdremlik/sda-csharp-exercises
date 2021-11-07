2f2af63cb0a30097225a60eb3b19cbf1fac370e4
Kacper Dremlik <kacper.dremlik@gmail.com>
2021-11-07 11:10:58 +0100

    Added branch E04 with feature Triangle.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj wysokość:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj znak");
+            char sign = Convert.ToChar(Console.ReadLine());
+
+
+
+            PrintTriangle(height, sign);
+        }
+
+        private static void PrintTriangle(int height, char sign)
+        {
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = height - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
+
