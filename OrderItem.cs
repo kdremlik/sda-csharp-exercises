7c6d45fe9ff78a5f28218a8d88c39d7b4e6b1c0c
Kacper Dremlik <kacper.dremlik@gmail.com>
2021-11-20 14:59:18 +0100

    WIP

---
+++ OrderItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class OrderItem
+    {
+        private string productName;
+        private int quantity;
+        private decimal unitPrice;
+
+        public OrderItem(string productName, int quantity, decimal unitPrice)
+        {
+            this.productName = productName;
+            this.quantity = quantity;
+            this.unitPrice = unitPrice;
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return productName;
+            }
+        }
+
+        public decimal UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+        }
+
+
+        public virtual decimal GetValue()
+        {
+            return quantity * unitPrice;
+        }
+        public override string ToString()
+        {
+            return $"{productName}, Cena: {unitPrice} PLN, {quantity} szt, wartość : {GetValue()} PLN";
+        }
+    }
+}
---
+++ OrderItemWithPackage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class OrderItemWithPackage : OrderItem
+    {
+        private string packageName;
+
+        public OrderItemWithPackage(string productName, int quantity, decimal unitPrice, string packageName, decimal packagePrice) : base(productName, quantity, unitPrice)
+        {
+            this.packageName = packageName;
+            this.PackagePrice = packagePrice;
+        }
+
+        protected decimal PackagePrice { get; }
+
+
+        public override decimal GetValue()
+        {
+            return base.GetValue() * (Quantity * PackagePrice);
+        }
+
+        public override string ToString()
+        {
+            return $"{ProductName}, cena: {UnitPrice} PLN, opakowanie: {packageName}, cena opakowania: {PackagePrice} PLN, {Quantity} szt, wartość: {} PLN"
+        }
+    }
+}
