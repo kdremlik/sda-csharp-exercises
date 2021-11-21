c778242ca86246b775313b89adb1eeeca649d191
Kacper Dremlik <kacper.dremlik@gmail.com>
2021-11-21 13:26:26 +0100

    Done without list.

--- LineSegment.cs
+++ LineSegment.cs
@@ -8,29 +8,43 @@ namespace sda_csharp_exercises
 {
     class LineSegment 
     {
-        public double X1 { get; }
-        public double Y1 { get; }
-        public double X2 { get; }
-        public double Y2 { get; }
+        private double X1 { get; }
+        private double Y1 { get; }
+        private double X2 { get; }
+        private double Y2 { get; }
 
-        private List<MyPoint> points = new List<MyPoint>();
+        //private List<MyPoint> points = new List<MyPoint>();
+        //public LineSegment(MyPoint a, MyPoint b)
+        //{
+        //    points.Add(a);
+        //    points.Add(b);
+        //}
+
         public LineSegment(MyPoint a, MyPoint b)
         {
-            points.Add(a);
-            points.Add(b);
+            X1 = a.X;
+            Y1 = a.Y;
+            X2 = b.X;
+            Y2 = b.Y;
         }
-
+        //public LineSegment(int x1, int y1, int x2, int y2)
+        //{
+        //    points.Add(new MyPoint(x1, y1));
+        //    points.Add(new MyPoint(x2, y2));
+        //}
         public LineSegment(int x1, int y1, int x2, int y2)
         {
-            points.Add(new MyPoint(x1, y1));
-            points.Add(new MyPoint(x2, y2));
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
         }
 
 
 
         public double Length()
         {
-            return Math.Sqrt(Math.Pow(points[1].X - points[0].X, 2) + Math.Pow(points[1].Y - points[0].Y, 2));
+            return Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
         }
 
 
